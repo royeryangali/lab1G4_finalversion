a0b6410e47a2606d71248612d849eb8b4787102a
Royer <r.yangali@pucp.pe>
2020-09-15 08:25:52 -0500

    royer gaa

---
+++ src/Usuario.java
@@ -0,0 +1,9 @@
+public class Usuario {
+    public int codigopucp;
+    public String nombre;
+    public String apellido;
+    public int telefono;
+    public int anexo;
+    public String departamento;
+
+}
