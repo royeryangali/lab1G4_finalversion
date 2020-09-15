fb6ba706891acee93e4bebe3fff820d1b6df2fb2
AlonsoGuevara15 <a20160679@pucp.edu.pe>
2020-09-15 08:46:20 -0500

    main

--- src/pe/pucp/edu/telecom/main/Main.java
+++ src/pe/pucp/edu/telecom/main/Main.java
@@ -1,8 +1,50 @@
 package pe.pucp.edu.telecom.main;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        String menu = "*****PUCPABook******" +
+                "\n1. Leer Datos" +
+                "\n2. Agregar Usuario" +
+                "\n3. Buscar Usuario" +
+                "\n4. Escribir Usuario" +
+                "\n-Opción: ";
+
+
+        Scanner sc = new Scanner(System.in);
+        int opt;
+        while (true) {
+
+            System.out.print(menu);
+
+            String numStr = sc.nextLine();
+
+            try {
+                opt = Integer.parseInt(numStr);
+
+            } catch (NumberFormatException e) {
+                System.out.println("POR FAVOR INGRESE UN NUMERO!!!!!!!\n");
+                continue;
+            }
+            switch (opt) {
+                case 1:
+
+                    break;
+                case 2:
+
+                    break;
+                case 3:
+
+                    break;
+                case 4:
+
+                    break;
+                default:
+                    System.out.println("OPCION ERRONEA!!!!!!!\n");
+                    break;
+            }
+        }
     }
-}
+    }
